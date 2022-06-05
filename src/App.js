a8e6eabf764171290a3e5d72bc3e0ae98ae362b6
Manish Kumar <manish7479dlp@gmail.com>
2022-06-05 11:59:32 +0530

    ADD: Name componet is added.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Title from "Components/Title/Title";
 import React from "react";
 import profileImgUrl from "images/profile.webp";
-import educationImgUrl from "images/education.webp"
-import skillImgUrl from "images/skill.webp"
-import experienceImgUrl from "images/experience.webp"
-import languageImgUrl from "images/language.webp"
-import hobbiesImgUrl from "images/hobbies.webp"
+import educationImgUrl from "images/education.webp";
+import skillImgUrl from "images/skill.webp";
+import experienceImgUrl from "images/experience.webp";
+import languageImgUrl from "images/language.webp";
+import hobbiesImgUrl from "images/hobbies.webp";
+import Name from "Components/Name/Name";
 
 const contentData = {
   profile: {
-    imgUrl : profileImgUrl,
+    imgUrl: profileImgUrl,
     title: "Profile",
     imgStyle: {
       background: "linear-gradient(48deg, rgb(220, 72, 155), rgb(243, 248, 7))",
@@ -19,9 +20,9 @@ const contentData = {
       background: "linear-gradient(to right ,rgb(243,248,7) , rgb(220,72,155))",
     },
   },
-  
+
   education: {
-    imgUrl : educationImgUrl,
+    imgUrl: educationImgUrl,
     title: "Education",
     imgStyle: {
       background: "linear-gradient(48deg, rgb(179,168,48), rgb(12,158,162))",
@@ -34,7 +35,7 @@ const contentData = {
   },
 
   skills: {
-    imgUrl : skillImgUrl,
+    imgUrl: skillImgUrl,
     title: "Skills",
     imgStyle: {
       background: "linear-gradient(48deg, rgb(42,181,228), rgb(228,90,109))",
@@ -47,36 +48,34 @@ const contentData = {
   },
 
   experience: {
-    imgUrl : experienceImgUrl,
+    imgUrl: experienceImgUrl,
     title: "Experience",
     imgStyle: {
       background: "linear-gradient(48deg, rgb(45,177,231), rgb(58,185,85))",
     },
     titleContainerStyle: {
       border: "3px solid rgb(184, 183, 183)",
-      background:
-        "linear-gradient(to right, rgb(58,185,85) , rgb(45,177,231))",
+      background: "linear-gradient(to right, rgb(58,185,85) , rgb(45,177,231))",
     },
   },
 
   language: {
-    imgUrl : languageImgUrl,
+    imgUrl: languageImgUrl,
     title: "Language",
     imgStyle: {
       background: "linear-gradient(48deg, rgb(12,75,123), rgb(149,179,81))",
     },
     titleContainerStyle: {
       border: "3px solid rgb(184, 183, 183)",
-      background:
-        "linear-gradient(to right, rgb(149,179,81) , rgb(12,75,123))",
+      background: "linear-gradient(to right, rgb(149,179,81) , rgb(12,75,123))",
     },
   },
 
   hobbies: {
-    imgUrl : hobbiesImgUrl,
+    imgUrl: hobbiesImgUrl,
     title: "hobbies",
     imgStyle: {
-      background: "linear-gradient(48deg, rgb(39,143,175), rgb(39,195,177))"
+      background: "linear-gradient(48deg, rgb(39,143,175), rgb(39,195,177))",
     },
     titleContainerStyle: {
       border: "3px solid rgb(184, 183, 183)",
@@ -91,6 +90,8 @@ const App = () => {
     contentData.education;
   return (
     <>
+      <Name />
+      
       <Title
         // title={"Profile"}
         imgUrl={contentData.profile.imgUrl}
@@ -133,7 +134,6 @@ const App = () => {
         imgStyle={imgStyle}
         titleContainerStyle={titleContainerStyle}
       />
-
     </>
   );
 };
---
+++ src/Components/Name/Name.js
@@ -0,0 +1,15 @@
+import "./Name.css";
+
+const Name = () => {
+  return (
+    <>
+      <div className="nameContainer"></div>
+      <div className="name">
+        <h1>Manish Kumar</h1>
+        <div className="underline"></div>
+      </div>
+    </>
+  );
+};
+
+export default Name;
