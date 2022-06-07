be5fecbb131cc7295c619d8c3d560b6a95a5b8b0
Manish Kumar <manish7479dlp@gmail.com>
2022-06-07 14:14:42 +0530

    Fix: unUsed variable is deleted.

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,14 +1,6 @@
 import "./Title.css";
 
-const Title = ({imgStyle ,titleContainerStyle, title, imgUrl}) => {
-  const temp = {
-    border: "3px solid rgb(184, 183, 183)",
-    background: "linear-gradient(to right ,rgb(243,248,7) , rgb(220,72,155))",
-  };
-
-//   console.log("x" , temp);
-//   console.log(titleContainerStyle);
-
+const Title = ({ imgStyle, titleContainerStyle, title, imgUrl }) => {
   return (
     <div className="mainContainer">
       <div className="iconContainer" style={imgStyle}>
