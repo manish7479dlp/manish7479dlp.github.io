455d40b7ec06f1245eab38a52c761dc75645a7dd
Manish Kumar <manish7479dlp@gmail.com>
2022-06-11 12:37:09 +0530

    ADD: Make Header content Responsive

--- src/Components/ContactInfo/ContactInfo.js
+++ src/Components/ContactInfo/ContactInfo.js
@@ -11,12 +11,12 @@ const ContactInfo = () => {
       <div className="contactInfoBox"></div>
       <div className=" contactContent">
           {/* <div className="center firstContactContent"> */}
-               <a className="first" href= {`mailto:${SocialMediaData.socialMediaLink.email}`} target= "_blank" rel="noreferrer"><img src= {emailUrl} alt = "email logo"/> Email</a>
-               <a href={SocialMediaData.socialMediaLink.instagram} target= "_blank" rel="noreferrer"><img src= {instagramUrl} alt = "instagram logo"/> Instagram</a>
+               <a className="first" href= {`mailto:${SocialMediaData.socialMediaLink.email}`} target= "_blank" rel="noreferrer"><img src= {emailUrl} alt = "email logo"/> <span className="displayToggle">Email</span></a>
+               <a href={SocialMediaData.socialMediaLink.instagram} target= "_blank" rel="noreferrer"><img src= {instagramUrl} alt = "instagram logo"/> <span className="displayToggle">Instagram</span></a>
           {/* </div> */}
           {/* <div className="center secondContactContent"> */}
-                <a className="first" href={SocialMediaData.socialMediaLink.github}target="_blank" rel="noreferrer"><img src= {githubUrl} alt = "github logo"/> Github</a>
-                <a href={SocialMediaData.socialMediaLink.linkedin} target= "_blank" rel="noreferrer"><img src= {linkedinUrl} alt = "linkedin logo"/> Linkedin</a>
+                <a className="first" href={SocialMediaData.socialMediaLink.github}target="_blank" rel="noreferrer"><img src= {githubUrl} alt = "github logo"/> <span className="displayToggle">Github</span></a>
+                <a href={SocialMediaData.socialMediaLink.linkedin} target= "_blank" rel="noreferrer"><img src= {linkedinUrl} alt = "linkedin logo"/> <span className="displayToggle">Linkedin</span></a>
           {/* </div> */}
       </div>
     </div>
